315e1ed37927dfed3ce19249834b6a004ccf0c7e
OneLousygamer <onelousygamer@github>
2021-05-19 09:25:23 +0100

    Initial commit!

---
+++ src/main/java/com/lousiesmods/lousiesutilities/LousiesUtilities.java
@@ -0,0 +1,41 @@
+package com.lousiesmods.lousiesutilities;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.*;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("lousiesutilities")
+public class LousiesUtilities
+{
+    public LousiesUtilities()
+    {
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event)
+    {
+
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+
+    }
+
+    private void processIMC(final InterModProcessEvent event)
+    {
+
+    }
+}
